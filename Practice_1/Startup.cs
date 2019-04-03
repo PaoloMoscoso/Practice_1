ebd177b42edf73425145dba7a8f1e13fae6e33a5
Paolo Moscoso <Paolo Moscoso@PMOSCOSOL-DH01>
2019-04-03 14:13:30 -0400

    Add project files.

---
+++ Practice_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practice_1.Startup))]
+namespace Practice_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
